67d454e9b0c34b9eb7ae47b5ba4e33d797643f05
LucaJahnen <luca.jahnen@gmail.com>
2024-06-08 17:45:28 +0200

    update styles

--- src/components/FirstSlide/FirstSlide.elements.jsx
+++ src/components/FirstSlide/FirstSlide.elements.jsx
@@ -1,7 +1,37 @@
 import styled from "styled-components";
+import bgImage from "../../assets/images/bg-sidebar-mobile.svg"
 
 export const SlideWrapper = styled.section`
 background-color: white;
 border-radius: 0.8rem;
 margin: 30% 5% 0 5%;
+`
+
+export const StepContainer = styled.div`
+background-image: url(${bgImage});
+background-repeat: no-repeat;
+background-size: 100%;
+width: 100%;
+padding: 5rem 0;
+position: absolute;
+top: 0;
+left: 0;
+z-index: -1;
+`
+
+export const StepsWrapper = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+gap: 1rem;
+`
+
+export const Step = styled.p`
+color: white;
+border-radius: 50%;
+border: 1px solid white;
+width: 2rem;
+height: 2rem;
+display: grid;
+place-items: center;
 `
--- src/components/FirstSlide/FirstSlide.jsx
+++ src/components/FirstSlide/FirstSlide.jsx
@@ -1,9 +1,16 @@
-import { SlideWrapper } from "./FirstSlide.elements"
+import { SlideWrapper, StepContainer, StepsWrapper, Step } from "./FirstSlide.elements"
 import Heading from "../components/Heading/Heading"
 
 const FirstSlide = () => {
   return (
     <SlideWrapper>
+        <StepContainer>
+          <StepsWrapper>
+            {Array(4).fill("").map((item, index) => {
+              return <Step>{index + 1}</Step>
+            })}
+          </StepsWrapper>
+        </StepContainer>
         <Heading text="Personal Info" />
     </SlideWrapper>
   )
