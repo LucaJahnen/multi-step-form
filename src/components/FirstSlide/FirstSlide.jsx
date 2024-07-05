25f172277962d79bfcca43dd0dad0294e14accc9
LucaJahnen <luca.jahnen@gmail.com>
2024-07-05 19:19:59 +0200

    configuring gh pages

--- src/components/FirstSlide/FirstSlide.jsx
+++ src/components/FirstSlide/FirstSlide.jsx
@@ -9,37 +9,35 @@ const FirstSlide = ({ data, updateData, errors }) => {
     <SlideContainer>
         <Heading>Personal Info</Heading>
         <Paragraph>Please provide your name, email address, and phone number.</Paragraph>
-        <div>
-          <InputGroup 
-            placeholder="e.g. Stephen King" 
-            id="name" label="Name" 
-            type="text" 
-            value={data.name} 
-            onChange={e => {updateData({name: e.target.value}); delete errors.name}}
-            error={errors.name != undefined}
-            errorMsg={errors.name}
-          />
-          <InputGroup 
-            placeholder="e. g. stephenking@lorem.com" 
-            id="email" 
-            label="Email Address" 
-            type="email" 
-            value={data.email} 
-            onChange={e => {updateData({email: e.target.value}); delete errors.email}}
-            error={errors.email != undefined}
-            errorMsg={errors.email}
-          />
-          <InputGroup 
-            placeholder="e. g. +1 234 567 890" 
-            id="phone" 
-            label="Phone Number" 
-            type="tel" 
-            value={data.phone}
-            onChange={e => {updateData({phone: e.target.value}); delete errors.phone}}
-            error={errors.phone != undefined}
-            errorMsg={errors.phone}
-          />
-        </div>
+        <InputGroup 
+          placeholder="e.g. Stephen King" 
+          id="name" label="Name" 
+          type="text" 
+          value={data.name} 
+          onChange={e => {updateData({name: e.target.value}); delete errors.name}}
+          error={errors.name != undefined}
+          errorMsg={errors.name}
+        />
+        <InputGroup 
+          placeholder="e. g. stephenking@lorem.com" 
+          id="email" 
+          label="Email Address" 
+          type="email" 
+          value={data.email} 
+          onChange={e => {updateData({email: e.target.value}); delete errors.email}}
+          error={errors.email != undefined}
+          errorMsg={errors.email}
+        />
+        <InputGroup 
+          placeholder="e. g. +1 234 567 890" 
+          id="phone"
+          label="Phone Number" 
+          type="tel" 
+          value={data.phone}
+          onChange={e => {updateData({phone: e.target.value}); delete errors.phone}}
+          error={errors.phone != undefined}
+          errorMsg={errors.phone}
+        /> 
     </SlideContainer>
   )
 }
--- src/components/components/Paragraph/Paragraph.elements.jsx
+++ src/components/components/Paragraph/Paragraph.elements.jsx
@@ -5,7 +5,6 @@ color: var(--cool-gray);
 margin: 0.5rem 0 1.4rem 0;
 font-weight: 400;
 line-height: 1.6;
-width: 95%;
 
 @media (min-width: 960px) {
     margin: 0.3rem 0 2.5rem 0;
